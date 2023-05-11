1d52309c1e1d178daddd64274d870fa74c8c3d82
Niriksh Bharti <111577030+Nirikshb@users.noreply.github.com>
2023-05-11 23:53:57 +0530

    solution

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Card from './Card';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Card Component Example</h1>
+      <Card
+        image={require('./save.jpg')}
+        title="Card Title 1"
+        escription="This is the description for Card 1"
+      />
+      <Card
+        image="image-url.jpg"
+        title="Card Title 2"
+        description="This is the description for Card 2"
+      />
+      <Card
+        image="logo.svg"
+        title="Card Title 3"
+        description="This is the description for Card 3"
+      />
     </div>
   );
-}
+};
 
 export default App;
